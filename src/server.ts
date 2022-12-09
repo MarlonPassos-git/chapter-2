9056acd1e718ad2baa0cfc6fe0bb64d1c3abe8cf
Marlon Passos <1marlonpassos@gmail.com>
2022-12-09 14:55:11 -0300

    feat: cofig debug

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,17 @@ import express from 'express';
 import { createCourse } from './routes';
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3333;
 console.log("cavalo")
 
 app.get("/", createCourse)
 app.get("/a", (req, res) => { 
+  console.log("cavalo")
+
+  const { name } = req.body
+  console.log(name)
   res.json({ message: "Hello World" })
 })
 
