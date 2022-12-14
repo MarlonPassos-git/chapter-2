1546e3ba5aed290979223c4fe2357d7754d3dcc8
Marlon Passos <1marlonpassos@gmail.com>
2022-12-14 00:28:15 -0300

    feat(router): base to import

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,24 @@
 import { Router } from 'express'
+import multer from 'multer'
+
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
 import { listCategoriesController } from '../modules/cars/useCases/listCategory'
 
 export const categoriesRoutes = Router()
+export const upload = multer({
+	dest: './temp',
+})
 
 categoriesRoutes.post(
 	'/',
 	createCategoryController.handle.bind(createCategoryController),
 )
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+	const { file } = request
+	console.log(file)
+	return response.send()
+})
 
 categoriesRoutes.get(
 	'/',
